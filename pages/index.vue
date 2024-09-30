787bf76b43ba77bd0ad1cfe6852ec261df7e63d5
unknown <rijal.1344@gmail.com>
2024-09-30 09:53:49 +0700

    page: add  login page

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+interface Form {
+  email: string;
+  password: string;
+}
+
+const form: Form = reactive({
+  email: "",
+  password: "",
+});
+
+const login = () => {
+  console.log(form);
+};
+</script>
+<template>
+  <NuxtLayout name="login-layout">
+    <form @submit.prevent="login()">
+      <div class="space-y-4">
+        <div>
+          <BaseInputLabel>Email</BaseInputLabel>
+          <BaseTextInput
+            type="email"
+            class="mt-1 block w-full"
+            v-model="form.email"
+            autofocus
+          />
+        </div>
+        <div>
+          <BaseInputLabel>Password</BaseInputLabel>
+          <BaseTextInput
+            type="password"
+            class="mt-1 block w-full"
+            v-model="form.password"
+            autofocus
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <div class="flex flex-col space-y-2">
+            <div>
+              <p class="text-sm">
+                Belum memiliki akun? klik
+                <NuxtLink
+                  class="text-blue-500 underline cursor-pointer"
+                  to="/register"
+                  >register</NuxtLink
+                >
+              </p>
+            </div>
+            <div>
+              <p class="text-sm">
+                Lupa password? klik
+                <NuxtLink
+                  class="text-blue-500 underline cursor-pointer"
+                  to="/reset"
+                  >reset password</NuxtLink
+                >
+              </p>
+            </div>
+          </div>
+
+          <BasePrimaryButton type="submit">Login</BasePrimaryButton>
+        </div>
+      </div>
+    </form>
+  </NuxtLayout>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    typography,
+    forms,
+    aspectRatio,
+  ],
+}
