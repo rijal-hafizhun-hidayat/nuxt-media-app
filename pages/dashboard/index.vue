fc3207b94e31b035edddf55ed708654032b8d7fc
unknown <rijal.1344@gmail.com>
2024-10-02 18:20:53 +0700

    page: add dashboard page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+console.log(true);
+</script>
+<template>
+  <NuxtLayout name="dashboard-layout">
+    <template #header>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Beranda
+          </h2>
+        </div>
+        <div>
+          <BasePrimaryButton type="button">Buat Postingan</BasePrimaryButton>
+        </div>
+      </div>
+    </template>
+  </NuxtLayout>
+</template>
