51e6fd425f961a5393cbad1d02f8a1d104b57254
unknown <rijal.1344@gmail.com>
2024-10-03 21:52:28 +0700

    api: integrate reset password api

--- pages/reset-password/index.vue
+++ pages/reset-password/index.vue
@@ -1,25 +1,70 @@
 <script setup lang="ts">
-console.log(true);
 interface Form {
   email: string;
 }
 
+const router = useRouter();
+const { $api } = useNuxtApp();
+const isLoading: Ref<boolean> = ref(false);
+const validation: Ref<any> = ref([]);
 const form: Form = reactive({
   email: "",
 });
-const resetPassword = () => {
-  console.log(form);
+
+const generateToken = async () => {
+  try {
+    isLoading.value = true;
+    const result = await $api("reset-password", {
+      method: "post",
+      body: {
+        email: form.email,
+      },
+    });
+
+    console.log(result);
+    validation.value = result;
+
+    return router.push({
+      name: "reset-password-update",
+      query: {
+        email: form.email,
+      },
+    });
+  } catch (error: any) {
+    isLoading.value = false;
+    validation.value = error.data;
+    console.log(validation.value);
+  } finally {
+    isLoading.value = false;
+  }
 };
 </script>
 <template>
   <NuxtLayout name="login-layout">
-    <form @submit.prevent="resetPassword()" class="space-y-4">
+    <BaseDangerAlert
+      v-if="validation.statusCode === 404"
+      :message="validation.errors"
+    />
+    <form @submit.prevent="generateToken()" class="space-y-4">
       <div>
         <BaseInputLabel>Masukkan Email</BaseInputLabel>
-        <BaseTextInput v-model="form.email" class="block w-full mt-1" />
+        <BaseTextInput
+          v-model="form.email"
+          type="email"
+          class="block w-full mt-1"
+        />
+        <BaseInputError
+          v-if="validation.statusCode === 400 && validation.errors.email"
+          :message="validation.errors.email._errors[0]"
+        />
       </div>
       <div>
-        <BasePrimaryButton type="submit">submit</BasePrimaryButton>
+        <BasePrimaryButton
+          :disabled="isLoading == true"
+          :class="{ 'opacity-75': isLoading == true }"
+          type="submit"
+          >submit</BasePrimaryButton
+        >
       </div>
     </form>
   </NuxtLayout>
