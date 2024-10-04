063104d4644bb77ac62f77ca4a596b155398f55b
unknown <rijal.1344@gmail.com>
2024-10-04 08:27:10 +0700

    page: add email, password and verified component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+  },
+});
