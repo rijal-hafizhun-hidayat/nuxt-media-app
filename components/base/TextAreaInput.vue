2fd993c87240cbbd62f668fca607d260cf89fa87
unknown <rijal.1344@gmail.com>
2024-10-08 09:33:50 +0700

    page: add create post page

---
+++ components/base/TextAreaInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(["update:modelValue"]);
+
+const input = ref(null);
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+<template>
+  <textarea
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+  ></textarea>
+</template>
