442689c5400a14321d46c803b101738c28444be4
unknown <rijal.1344@gmail.com>
2024-10-16 11:42:42 +0700

    page: add header title in create post page

--- pages/dashboard/create.vue
+++ pages/dashboard/create.vue
@@ -25,6 +25,15 @@ const send = async () => {
 </script>
 <template>
   <NuxtLayout name="dashboard-layout">
+    <template #header>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Tambah Postingan
+          </h2>
+        </div>
+      </div>
+    </template>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
         <BaseSuccessAlert
