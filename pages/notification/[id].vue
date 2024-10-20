1223af85a58ff1a1fc99d03617986d74379ab70e
unknown <rijal.1344@gmail.com>
2024-10-20 18:51:38 +0700

    api: integrate api update notification is_read

---
+++ pages/notification/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth-middleware"],
+});
+const route = useRoute();
+console.log(route.params.id);
+</script>
+<template>
+  <NuxtLayout name="dashboard-layout">
+    <template #header>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Detail Notifikasi
+          </h2>
+        </div>
+      </div>
+    </template>
+  </NuxtLayout>
+</template>
